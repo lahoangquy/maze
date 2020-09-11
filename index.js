2b1c0a51878d421a1b03de224866bc855918c9f3
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2020-09-11 12:16:21 +0930

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 14;
-const cellsVertical = 10;
+const cellsHorizontal = 30;
+const cellsVertical = 25;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
